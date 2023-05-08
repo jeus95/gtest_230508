d193bf91e2f71a8c7348dd9296f071d108e38732
Chansik Yun <chansigi@gmail.com>
2023-05-08 05:47:01 +0000

    .

--- 4_스위트픽스쳐.cpp
+++ 4_스위트픽스쳐.cpp
@@ -16,6 +16,38 @@ public:
 //------
 #include <gtest/gtest.h>
 
+class TerminalTest : public testing::Test {
+protected:
+  Terminal* ts = nullptr;
+
+  void SetUp() override
+  {
+    ts = new Terminal;
+    ts->Connect();
+  }
+
+  void TearDown() override
+  {
+    ts->Disconnect();
+    delete ts;
+  }
+};
+
+TEST_F(TerminalTest, Login)
+{
+    ts->Login("test_id", "test_password");
+
+    ASSERT_TRUE(ts->IsLogin()) << "로그인 하였을 때";
+}
+
+TEST_F(TerminalTest, Logout)
+{
+    ts->Login("test_id", "test_password");
+    ts->Logout();
+
+    ASSERT_FALSE(ts->IsLogin()) << "로그아웃 하였을 때";
+}
+
 // 아래의 코드를 암묵적 설치 / 해체를 이용해서 변경해보세요.
 #if 0
 TEST(TerminalTest, Login)
