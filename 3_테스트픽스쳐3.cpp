0e7fc4175a222b8ba30ad819deeaae59216cffe8
Chansik Yun <chansigi@gmail.com>
2023-05-08 05:06:30 +0000

    .

--- 3_테스트픽스쳐3.cpp
+++ 3_테스트픽스쳐3.cpp
@@ -4,7 +4,7 @@
 //  - CUT(Class Under Test / Code Under Test)
 class Calc {
 public:
-    // Calc(int n) { }
+    Calc(int n) { }
 
     double Display() { return 0.0; }
 
@@ -17,14 +17,27 @@ public:
 #include <gtest/gtest.h>
 
 // 2. Test Fixture를 설치하는 방법 3가지
-
+//  3) Implicit Set Up(암묵적 설치)
+//   => xUnit Test Framework이 제공하는 기능입니다.
+//   => 여러 테스트케이스에서 같은 테스트 픽스쳐 설치의 코드를 암묵적으로 호출되는
+//      함수를 통해서 처리합니다.
+//    > 명시적인 테스트 스위트 클래스를 정의해야 합니다.
 #define SPEC(msg) printf(msg "\n")
 
-TEST(CalcTest, PressPlus)
+class CalcTest : public testing::Test {
+protected:
+    Calc* calc = nullptr;
+
+    void SetUp() override
+    {
+        std::cout << "SetUp" << std::endl;
+        calc = new Calc(0);
+    }
+};
+
+TEST_F(CalcTest, PressPlus)
 {
     SPEC("2 더하기 2를 하였을 때, 결과가 4가 나오지는 여부를 검증합니다.");
-    // Arrange
-    Calc* calc = new Calc;
 
     // Act
     calc->Enter(2);
@@ -40,11 +53,9 @@ TEST(CalcTest, PressPlus)
     }
 }
 
-TEST(CalcTest, PressPlus2)
+TEST_F(CalcTest, PressPlus2)
 {
     SPEC("2 더하기 2를 하였을 때, 결과가 4가 나오지는 여부를 검증합니다.");
-    // Arrange
-    Calc* calc = new Calc;
 
     // Act
     calc->Enter(2);
