4e8875407b98c786a46948b8c0adf24fe71bbc7d
Chansik Yun <chansigi@gmail.com>
2023-05-08 02:20:08 +0000

    .

--- 1_시작.cpp
+++ 1_시작.cpp
@@ -3,10 +3,12 @@
 
 // 아래의 코드는 항상 동일합니다.
 // > main 함수를 라이브러리에 포함시켜서 사용하는 경우가 있습니다.
+#if 0
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+#endif
 
 // $ g++ 1_시작.cpp -I ./googletest/googletest/include -L. -lgtest -pthread
---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+g++ $1 -I ./googletest/googletest/include -L. -lgtest -pthread
