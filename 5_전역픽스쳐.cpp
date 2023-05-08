6c6bdcbd0082de50b2bdaf26cec5e07e8fb99da2
Chansik Yun <chansigi@gmail.com>
2023-05-08 06:46:20 +0000

    .

--- 5_전역픽스쳐.cpp
+++ 5_전역픽스쳐.cpp
@@ -22,17 +22,69 @@ public:
     }
 };
 
+class MyTestEnvironment2 : public testing::Environment {
+public:
+    void SetUp() override
+    {
+        std::cout << "라이브러리2 초기화" << std::endl;
+    }
+
+    void TearDown() override
+    {
+        std::cout << "라이브러리2 정리" << std::endl;
+    }
+};
+
 TEST(SampleTest, foo) { }
 TEST(SampleTest, goo) { }
 
 TEST(ImageTest, Resize) { }
 TEST(ImageTest, Blur) { }
 
-// 2) 전역 픽스쳐를 설치하는 방법
-//   1. 전역 변수
-testing::Environment* my_env = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+// 미정의 동작(Undefined)
+TEST(SampleTest, hoo)
+{
+    int n = 1;
+    int r = ++n + ++n; // 미정의 동작, 컴파일러마다 결과가 달라집니다.
+    std::cout << r << std::endl;
+}
 
-//   2. main 함수
+int foo()
+{
+    std::cout << "foo" << std::endl;
+    return 10;
+}
+int goo()
+{
+    std::cout << "goo" << std::endl;
+    return 20;
+}
+
+void hoo(int a, int b) { }
+
+// 미지정 동작(Unspecified)
+TEST(SampleTest, koo)
+{
+    int rf = foo();
+    int rg = goo();
+    hoo(rf, rg);
+
+    // hoo(foo(), goo()); // 인자로 전달된 함수의 호출 순서가 미지정입니다.
+}
+
+// 2) 전역 픽스쳐를 설치하는 방법
+//  =>  main을 직접 정의해서 사용하는 것이 좋습니다.
+
+//  1. 전역 변수
+//   > 전역 변수의 초기화 순서가 파일별로 순서가 정의되지 않습니다.
+//    => C++ 미지정 동작
+// a.cpp
+testing::Environment* my_env1 = testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+
+// b.cpp
+testing::Environment* my_env2 = testing::AddGlobalTestEnvironment(new MyTestEnvironment2);
+
+//  2. main 함수
 #if 0
 int main(int argc, char** argv)
 {
@@ -40,6 +92,7 @@ int main(int argc, char** argv)
 
     // 주의사항: new로 생성해서 전달해야 합니다.
     testing::AddGlobalTestEnvironment(new MyTestEnvironment);
+    testing::AddGlobalTestEnvironment(new MyTestEnvironment2);
 
     return RUN_ALL_TESTS();
 }
