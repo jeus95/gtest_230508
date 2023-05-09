9061b9bec4c250e456ad91f6c61d4b78c2eeae46
Chansik Yun <chansigi@gmail.com>
2023-05-09 05:11:28 +0000

    .

--- 11_테스트리스너.cpp
+++ 11_테스트리스너.cpp
@@ -25,27 +25,63 @@ public:
     {
         std::cout << __func__ << std::endl;
     }
-    void OnEnvironmentsSetUpStart(const UnitTest& /*unit_test*/) override { std::cout << __func__ << std::endl; }
-    void OnEnvironmentsSetUpEnd(const UnitTest& /*unit_test*/) override { std::cout << __func__ << std::endl; }
-    void OnTestSuiteStart(const TestSuite& /*test_suite*/) override { std::cout << __func__ << std::endl; }
+    void OnEnvironmentsSetUpStart(const UnitTest& /*unit_test*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void OnEnvironmentsSetUpEnd(const UnitTest& /*unit_test*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void OnTestSuiteStart(const TestSuite& /*test_suite*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
 
     void OnTestStart(const TestInfo& /*test_info*/) override
     {
         std::cout << __func__ << std::endl;
     }
-    void OnTestDisabled(const TestInfo& /*test_info*/) override { std::cout << __func__ << std::endl; }
-    void OnTestPartResult(const TestPartResult& /*test_part_result*/) override { std::cout << __func__ << std::endl; }
-    void OnTestEnd(const TestInfo& /*test_info*/) override { std::cout << __func__ << std::endl; }
-    void OnTestSuiteEnd(const TestSuite& /*test_suite*/) override { std::cout << __func__ << std::endl; }
+    void OnTestDisabled(const TestInfo& /*test_info*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void OnTestPartResult(const TestPartResult& /*test_part_result*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
+
+    void OnTestEnd(const TestInfo& test_info) override
+    {
+        std::cout << __func__ << std::endl;
+        std::cout
+            << test_info.test_suite_name() << "." << test_info.name() << " --> "
+            << (test_info.result()->Passed() ? "PASS" : "FAIL")
+            << std::endl;
+    }
+
+    void OnTestSuiteEnd(const TestSuite& /*test_suite*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
 
     void OnEnvironmentsTearDownStart(const UnitTest& /*unit_test*/) override
     {
         std::cout << __func__ << std::endl;
     }
-    void OnEnvironmentsTearDownEnd(const UnitTest& /*unit_test*/) override { std::cout << __func__ << std::endl; }
+    void OnEnvironmentsTearDownEnd(const UnitTest& /*unit_test*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
     void OnTestIterationEnd(const UnitTest& /*unit_test*/,
-        int /*iteration*/) override { std::cout << __func__ << std::endl; }
-    void OnTestProgramEnd(const UnitTest& /*unit_test*/) override { std::cout << __func__ << std::endl; }
+        int /*iteration*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void OnTestProgramEnd(const UnitTest& /*unit_test*/) override
+    {
+        std::cout << __func__ << std::endl;
+    }
 };
 
 // 2. 이벤트 리스너를 등록하는 방법
@@ -55,6 +91,9 @@ int main(int argc, char** argv)
     testing::InitGoogleTest(&argc, argv);
 
     testing::TestEventListeners& listenerns = UnitTest::GetInstance()->listeners();
+    // 기본 프린터를 제거할 수 있습니다.
+    delete listenerns.Release(listenerns.default_result_printer());
+
     listenerns.Append(new MyTestEventListener);
 
     return RUN_ALL_TESTS();
