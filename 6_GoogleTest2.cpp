0e643e315384ac4992c80144a82dc7207d415e45
Chansik Yun <chansigi@gmail.com>
2023-05-09 00:18:40 +0000

    .

--- 6_GoogleTest2.cpp
+++ 6_GoogleTest2.cpp
@@ -26,3 +26,24 @@ TEST(ImageTest, hoo) { }
 TEST(ImageProcessorTest, foo) { }
 TEST(ImageProcessorTest, goo) { }
 TEST(ImageProcessorTest, hoo) { }
+
+//----
+// 7. 반복 테스트 / 순서 무작위
+//  => "변덕스러운 테스트"를 확인할 수 있습니다.
+// $ ./a.out --gtest_repeat=10
+// $ ./a.out --gtest_repeat=10 --gtest_shuffle
+// $ ./a.out --gtest_repeat=10 --gtest_shuffle --gtest_break_on_failure
+
+int cnt = 0;
+int GetCount() { return ++cnt; }
+
+TEST(SampleTest, Sample1)
+{
+    EXPECT_EQ(GetCount(), 1);
+}
+
+TEST(SampleTest, Sample2)
+{
+    EXPECT_EQ(GetCount(), 2);
+    cnt = 0;
+}
