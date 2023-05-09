81db46c5aaf1d2e8715834ce826c8ef4c9d2b128
Chansik Yun <chansigi@gmail.com>
2023-05-09 00:11:46 +0000

    .

--- 6_GoogleTest2.cpp
+++ 6_GoogleTest2.cpp
@@ -4,6 +4,9 @@
 // 6. 테스트 필터
 // > 원하는 테스트만 선택적으로 수행할 수 있습니다.
 // : 테스트의 이름을 잘 구성해야 합니다.
+// - P_foo
+// - N_foo
+// - I_foo
 
 // $ ./a.out --gtest_filter=ImageTest.foo
 // $ ./a.out --gtest_filter=ImageTest.foo:ImageProcessorTest.goo
